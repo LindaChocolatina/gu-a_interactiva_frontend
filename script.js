5aace567b6311a76fcecdf6db2af32d114fcc25f
LindaChocolatina <lindasioc@gmail.com>
2026-04-08 12:04:26 -0500

    screenshots, css y js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Se ejecuta cuando el DOM está listo
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM cargado correctamente 🚀");
+
+    // Prueba visible (opcional)
+    alert("Tu JavaScript está funcionando ✨");
+});
